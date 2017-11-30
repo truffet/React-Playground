3b01f771c23f8fcf683fc2e2082ce9817329d1c0
sleepy <truffet22@gmail.com>
2017-11-30 17:19:39 +0100

    added shell script for config such as babel and edited text in readme

---
+++ config.sh
@@ -0,0 +1,13 @@
+sudo npm install -g babel
+sudo npm install -g babel-cli
+sudo npm install -g npm-add-script
+sudo npm install --save react
+sudo npm install --save react-dom
+sudo npm install --save react-router
+sudo npm install --save react-router-dom
+sudo npm install --save webpack
+sudo npm install --save webpack-dev-server
+sudo npm install babel-core
+sudo npm install babel-loader
+sudo npm install babel-preset-react
+sudo npm install babel-preset-es2015
